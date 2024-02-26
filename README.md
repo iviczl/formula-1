339020f1f3168a4b42a00889286d1502e85f19a2
László Ivicz <ivicz.laszlo.dr@gmail.com>
2024-02-26 14:02:01 +0100

    API endpoints

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# React + TypeScript + Vite
+# UI: React + TypeScript + Vite
+
+# API: Express (Node) + TypeScript
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,77 @@
 import express, { Express, Request, Response } from 'express'
-// const express = require('express')
+import { readFile } from 'fs/promises'
 
-const app: Express = express()
 const port = 8000
+const app: Express = express()
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello')
+const shuffleArray = (array: number[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+}
+
+type Driver = {
+  id: number
+  code: string
+  firstname: string
+  lastname: string
+  country: string
+  team: string
+  imgUrl: string
+  place: number
+}
+
+let drivers: Driver[]
+const loadDrivers = async () => {
+  try {
+    const rawData = JSON.parse((await readFile('./db/drivers.json')).toString())
+    drivers = rawData.map((item: any) => ({
+      ...item,
+      imgUrl: `/static/${(item.code as string).toLowerCase()}.png`,
+      place: 0,
+    }))
+
+    let orders: number[] = []
+    for (let i = 1; i <= drivers.length; i++) {
+      orders.push(i)
+    }
+    shuffleArray(orders)
+    drivers.forEach((driver) => (driver.place = orders.pop() ?? 0))
+    drivers.sort((a, b) => a.place - b.place)
+  } catch (error) {
+    console.error(`Error when trying to read the file: {error.message}`)
+  }
+}
+loadDrivers()
+
+app.get('/api/drivers', async (req: Request, res: Response) => {
+  res.send(drivers)
 })
 
-app.get('/hi', (req: Request, res: Response) => {
-  res.send('hi2')
+app.post('/api/drivers/:driverId/overtake', (req: Request, res: Response) => {
+  const driverId = parseInt(req.params.driverId)
+  const driver = drivers.find((driver) => driver.id === driverId)
+  let succeeded = false
+
+  if (driver && driver.place > 1) {
+    const place = driver.place
+    const otherPlace = place - 1
+    const otherDriver = drivers.find((driver) => driver.place === otherPlace)
+    if (otherDriver) {
+      driver.place = otherPlace
+      otherDriver.place = place
+      drivers.sort((a, b) => a.place - b.place)
+      succeeded = true
+    }
+  }
+  res.send(succeeded)
 })
+
+app.use('/static', express.static('image'))
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}.`)
+  console.log('service started')
 })
